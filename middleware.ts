be3ec2638d18eaa7cc9963c246143b9ca48fc393
DanGrigorescu <Grigorescu.danioan@gmail.com>
2024-04-02 13:26:28 +0300

    clerk webhooks

--- middleware.ts
+++ middleware.ts
@@ -9,6 +9,8 @@ export default authMiddleware({
   // Prevent the specified routes from accessing
   // authentication information:
   // ignoredRoutes: ['/no-auth-in-this-route'],
+
+  publicRoutes: ["/api/webhooks.clerk"],
 });
 
 export const config = {
