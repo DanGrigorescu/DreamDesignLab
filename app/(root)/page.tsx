51eccb963521bf6ca1076ffc5ee677a0ff1b466c
DanGrigorescu <Grigorescu.danioan@gmail.com>
2024-05-21 15:17:40 +0300

    home page fix

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -17,7 +17,7 @@ const Home = async ({ searchParams }: SearchParamProps) => {
           Cultivate Your Imagination with DreamDesignLab
         </h1>
         <ul className="flex-center w-full gap-20">
-          {navLinks.slice(1, 5).map((link) => (
+          {navLinks.slice(1, 6).map((link) => (
             <Link
               key={link.route}
               href={link.route}
